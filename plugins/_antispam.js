75924b5ed35d10f4acb22c486ffe7de6b2f3988b
Morales <moralesjuancarlo41@gmail.com>
2025-01-30 21:48:41 -0500

    Actualizar _antispam.js

--- plugins/_antispam.js
+++ plugins/_antispam.js
@@ -35,7 +35,7 @@ const timeDifference = currentTime - userData.lastMessageTime
 if (userData.antiBan === 1) {
 if (userData.message < 1) {
 userData.message++  
-motive = `᥀·࣭࣪̇˖⚔️◗ 𝙉𝙤 𝙝𝙖𝙜𝙖𝙨 𝙨𝙥𝙖𝙢.`
+motive = `⚔️ 𝙉𝙤 𝙝𝙖𝙜𝙖𝙨 𝙨𝙥𝙖𝙢.`
 await conn.reply(m.chat, motive, m, { mentions: [m.sender] })  
 user.messageSpam = motive
 }} else if (userData.antiBan === 2) {
