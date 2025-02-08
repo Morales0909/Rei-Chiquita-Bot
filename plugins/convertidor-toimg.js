899a8e648d24a371ec364283f714dc20b3f5e873
Morales <moralesjuancarlo41@gmail.com>
2025-02-08 15:11:10 -0500

    Actualizar convertidor-toimg.js

--- plugins/convertidor-toimg.js
+++ plugins/convertidor-toimg.js
@@ -2,7 +2,7 @@ import { webp2png } from '../lib/webp2mp4.js'
 
 var handler = async (m, { conn, usedPrefix, command }) => {
 
-const notStickerMessage = `*🌳 Responda a una imagen*`
+const notStickerMessage = `*Responda a una imagen :v*`
 if (!m.quoted) throw notStickerMessage 
 const q = m.quoted || m
 let mime = q.mediaType || ''
