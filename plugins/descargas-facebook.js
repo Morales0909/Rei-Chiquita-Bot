3da7ae5a8836539cd82f20c4f6e554c9b180037e
Morales <moralesjuancarlo41@gmail.com>
2025-02-08 15:16:31 -0500

    Actualizar descargas-facebook.js

--- plugins/descargas-facebook.js
+++ plugins/descargas-facebook.js
@@ -2,10 +2,10 @@
 
 const handler = async (m, { text, conn, args, usedPrefix, command }) => {
   if (!args[0]) {
-    return conn.reply(m.chat, '*\`Ingresa El link Del vídeo a descargar 🤍\`*', m, fake);
+    return conn.reply(m.chat, '*\`🤓☝🏻 Ingresa El link Del vídeo a descargar\`*', m, fake);
   }
 
-  await m.react('🕒');
+  await m.react('⏳');
   let res;
   try {
     res = await igdl(args[0]);
