fe17d27d79f662eeac5b2a5e4790ff0dfd9026f5
Morales <moralesjuancarlo41@gmail.com>
2025-02-09 11:00:31 -0500

    Actualizar descargas-vimeo.js

--- plugins/descargas-vimeo.js
+++ plugins/descargas-vimeo.js
@@ -3,11 +3,11 @@ import fetch from "node-fetch"
 let handler = async (m, { conn, args, usedPrefix, command }) => {
   if (!args[0]) {
     await m.react('✖️')
-    return m.reply("☁️ ingresa el link de un video de Vimeo")
+    return m.reply("🤓☝🏻 ingresa el link de un video de Vimeo")
   }
 
   try {
-    await m.react('🕒')
+    await m.react('⏳')
 
     let api = await fetch(`https://apis-starlights-team.koyeb.app/starlight/vimeo-DL?url=${args[0]}`)
     let { title, duration, thumbnail, medias } = await api.json()
