0f1eef95aa542614ebbc95857a873e00e178a9b0
Morales <moralesjuancarlo41@gmail.com>
2025-02-09 10:49:59 -0500

    Actualizar descargas-tiktok-mp3.js

--- plugins/descargas-tiktok-mp3.js
+++ plugins/descargas-tiktok-mp3.js
@@ -3,7 +3,7 @@ import yts from 'yt-search'
 
 let handler = async (m, { conn, text, usedPrefix, command }) => {
 if (!text) throw m.reply(`Ingresa un link de Tiktok\n*✧ Ejemplo:* ${usedPrefix}${command} https://vm.tiktok.com/ZMhAk8tLx/`);
-conn.sendMessage(m.chat, { react: { text: "🕒", key: m.key } });
+conn.sendMessage(m.chat, { react: { text: "⏳", key: m.key } });
 
   let d2 = await fetch(`https://eliasar-yt-api.vercel.app/api/search/tiktok?query=${text}`)
   let dp = await d2.json()
